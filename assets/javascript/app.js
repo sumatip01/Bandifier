686c93b18f56d821982caeb913b1ef985bf88450
Simon Cain <rudenik@gmail.com>
2018-05-28 21:50:02 -0400

    pushing some changes to the github.io site so that it might actually authenticate properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,86 @@
+// var spotifyAPI = new SpotifyWebAPI();
+// spotifyAPI.setAccessToken('')
+
+var clientId = 'fe8362d03fae494c914dbed629a6f9f8';
+var clientSecret = 'b3e9bee2e7df4d8f832ff04dc8e2cfa0';
+var encodedData = window.btoa(clientId + ':' + clientSecret);
+var authorizationHeaderString = 'Authorization: Basic ' + encodedData;
+
+
+
+
+
+getAuthorization();
+
+function getAuthorization(){
+    // $.ajax({
+    //     url: 'https://accounts.spotify.com/api/token',
+    //     method: 'POST',
+    //     'content-type': "application/x-www-form-urlencoded",
+    //     header: {
+    //             Authorization: authorizationHeaderString,
+    //         },
+    //     data:{
+    //         grant_type: "client_credentials",
+    //     }
+    //     }).then(function(spotResp){
+    //     console.log("Spot Auth resp");
+    //     console.log(spotResp);
+    // })
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = 'fe8362d03fae494c914dbed629a6f9f8';
+const redirectUri = 'https://rudenik.github.io/Bandifier/';
+const scopes = [
+  'user-top-read'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
+}
+
+$.ajax({
+    url: "https://api.spotify.com/v1/me/top/artists",
+    type: "GET",
+    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+    success: function(data) { 
+      // Do something with the returned data
+      data.items.map(function(artist) {
+        let item = $('<li>' + artist.name + '</li>');
+        item.appendTo($('#top-artists'));
+      });
+    }
+ });
+ 
+ 
+
+
+
+
+
+
+}
+
 $('#searchbutton').click(function () {
+    var searchTerm = $("#search-input").val();
+    sessionStorage.setItem("searchedTerm", searchTerm);
     $.ajax({
         url: 'https://en.wikipedia.org/w/api.php',
         data: {
@@ -33,27 +115,32 @@ $('#searchbutton').click(function () {
         //   }
         console.log(resp);
     });
+    
     console.log("button clicked")
-    // $.ajax({
-    //     url: 'http://en.wikipedia.org/w/api.php',
-    //     data: {
-    //         action: 'parse',
-    //         page: 'API:Parsing_wikitext',
-    //         srsearch: $("#search-input").val(),
-    //         prop: 'sections|text',
-    //         section: 1,
-    //     },
-    //     dataType: "json",
+    $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php',
+        data: {
+            action: 'parse',
+            format: 'json',
+            srsearch: searchTerm,
+            prop:"revisions",
+            rvprop: "content",
+            origin: "github.io"
+            // prop: 'sections|text',
+            // section: 0
+        },
+        dataType: "json",
         
             
 
-    // }).then(function(resp2){
-    //     console.log(resp2);
-    // });
+    }).then(function(resp2){
+        console.log(resp2);
+    });
 });
 
 function processResult(apiResult) {
     // for (var i = 0; i < apiResult.query.search.length; i++) {
+        $('#display-result').append('<img src='+apiResult.query.pages[apiResult.query.search[0].pageid].thumbnail.source+'>')
         $('#display-result').append('<p>' + apiResult.query.pages[apiResult.query.search[0].pageid].extract + '</p>');
         // $('#display-result').append('<p>' + apiResult.query.search[i].snippet + '</p>');
     // }
