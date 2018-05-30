7ae7cea6207e5d25ca8859982a0f9f07853b5d69
Sang <leesanxi@gmail.com>
2018-05-29 21:09:32 -0400

    lyrics done

--- testing/lyrics.js
+++ testing/lyrics.js
@@ -3,13 +3,45 @@ var trackID;
 var track;
 var artistName;
 var APIkey="003b119f7901034749c7fd78d8ea9bfc";
+// get artist from search
+var artist= sessionStorage.getItem("searchedTerm");
 
-$(".track-submit").on("click",function(){
-    track=$(".lyric-input").val();
+//get tracks
+//http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc
+$(".artist-submit").on("click",function(){
+    artist=$(".artist-input").val();
     //must replace space with %20 for query
+    artist=artist.replace(/ /g,"%20");
+
+    var queryUrl="https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&page_size=100&q_artist="+artist+"&apikey=003b119f7901034749c7fd78d8ea9bfc";
+    $.ajax({
+        url:queryUrl,
+        dataType:"jsonp",
+        method:"GET",
+        
+    })
+    .then(function(response){
+        var arr=response.message.body.track_list;
+        var s=$("<select>")
+        var t=$("<form>")
+        t.appendTo(".tracks-result")
+        s.attr("name","tracks").appendTo(t);
+        for(i=0;i<arr.length;i++){
+            console.log(arr[i].track.track_name);
+            $("<option>").attr("value",arr[i].track.track_name).text(arr[i].track.track_name).appendTo(s);
+        }
+    })
+
+})
+
+//show lyrics on track change
+
+$( ".tracks-result" ).on("change","select",function () {
+      track=$(this).val();
+    //must replace space with %20 for query
+    console.log(track);
     track=track.replace(/ /g,"%20");
     console.log(track);
-
     var queryUrl="https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track="+track+"&quorum_factor=1&apikey="+APIkey;
     $.ajax({
         url:queryUrl,
@@ -36,39 +68,5 @@ $(".track-submit").on("click",function(){
         })
     
     });
-    $("lyric-input").text("");
-
-
-
-})
-//get tracks
-//http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc
-$(".artist-submit").on("click",function(){
-    artist=$(".artist-input").val();
-    //must replace space with %20 for query
-    artist=artist.replace(/ /g,"%20");
-
-    var queryUrl="https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_artist="+artist+"&apikey=003b119f7901034749c7fd78d8ea9bfc";
-    $.ajax({
-        url:queryUrl,
-        dataType:"jsonp",
-        method:"GET",
-        
-    })
-    .then(function(response){
-        var arr=response.message.body.track_list
-        for(i=0;i<arr.length;i++){
-            console.log(arr[i].track.track_name);
-        }
-    })
-
-})
-
-/* 
-<select name="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="fiat">Fiat</option>
-  <option value="audi">Audi</option>
-</select>
-*/
+    
+  });
