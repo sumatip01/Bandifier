575c777c35e9c63e56743fd7e082276cfb449d82
Sang <leesanxi@gmail.com>
2018-05-31 21:44:40 -0400

    added youtube

--- testing/lyrics.js
+++ testing/lyrics.js
@@ -2,6 +2,7 @@ $('.dropdown-trigger').dropdown();
 
 var setLyrics = {
     trackID: "",
+    track:"",
     APIkey: "003b119f7901034749c7fd78d8ea9bfc",
     artist: sessionStorage.getItem("searchedTerm"),//"the strokes",
     getArtist: function () {
@@ -34,6 +35,7 @@ var setLyrics = {
 
         $(".tracks-dropdown").on("click", "span", function () {
             var track = $(this).text();
+            setLyrics.track=track;
             // track = track.replace(/ /g, "%20");
             track = encodeURI(track)
             console.log(track);
@@ -62,7 +64,23 @@ var setLyrics = {
                             $("<h5>").text(decodeURI(track)).appendTo(".lyrics");
                             $("<p>").text(str).appendTo(".lyrics");
                         })
-                });
+                })
+                .then(function(){
+                    
+                    var query=setLyrics.track;
+                    var APIkey="AIzaSyB5lIeHvKSoj0JCXpVRpfwrz0WKP0vYhKc";
+                    queryURL="https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true&videoLicense=creativeCommon&q="+encodeURI(query+" "+setLyrics.artist)+"&type=music&key="+APIkey;
+                   
+                    $.ajax({
+                        method:"GET",
+                        url:queryURL,
+                    }).then(function(response){
+                        console.log(response.items[0]);
+                        
+                        $("<iframe>").css({width:"100%", height:"110%"}).attr("frameborder",0).attr("allow","autoplay; encrypted-media").attr("src","https://www.youtube.com/embed/"+response.items[0].id.videoId).appendTo(".youtube-container");
+                    });
+
+                })
         });
     }
 }
